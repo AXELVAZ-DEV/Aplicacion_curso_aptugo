5d77d8436ccfeb9abb8e7e2d55de6231a58016d7
AXELVAZ-DEV <axelv6642@gmail.com>
2024-01-16 20:29:59 -0600

    terminando la estructura del portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <div>
       < Header />
+      <Contenido />
       <Footer />
     </div>
   )
@@ -24,28 +25,44 @@ function App() {
     )
   }
 
-  function MainPrincipal(){
-    return(
-      <main>
-        <h1><button className='tec'>Mis tecnologias</button></h1>
-        <div className='contenedores'>
-          <div className='contenedor1'>informacion</div>
-          <div className='contenedor2'>imagenes</div>
-        </div>
-      </main>
-    )
-  }
+  function Contenido(){
 
-  function MainAlternativo(){
+    const  [mostrarMain1, setMostrarMain1] = useState(true)
+
+    const mostrarMain = () => {
+      setMostrarMain1((prevMostrarMain1) => !prevMostrarMain1)
+    }
+
+    function MainPrincipal(){
+      return(
+        <main onClick={mostrarMain} className='info'>
+          <h1><button className='tec'>Mis tecnologias</button></h1>
+          <div className='contenedores'>
+            <div className='contenedor1'>informacion</div>
+            <div className='contenedor2'>imagenes</div>
+          </div>
+        </main>
+      )
+    }
+  
+    function MainAlternativo(){
+      return(
+        <main className='info'>
+          <h1><button onClick={mostrarMain} className='tec'>Lo que se</button></h1>
+          <div className='contenedores'>
+            <div className='contenedor1'>informacion alternativa</div>
+            <div className='contenedor2'>imagenes alternativas</div>
+          </div>
+        </main>
+      )
+    }
+
     return(
       <main>
-        <h1><button className='tec'>Mis tecnologias</button></h1>
-        <div className='contenedores'>
-          <div className='contenedor1'>informacion</div>
-          <div className='contenedor2'>imagenes</div>
-        </div>
+        {mostrarMain1 ? <MainPrincipal /> : <MainAlternativo />}
       </main>
     )
+
   }
 
   function Footer() {
